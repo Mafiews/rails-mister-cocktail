8c4701386c2283e946390e5e6bcd9cb7fb22b5eb
Matthieu Minier <minier.matthieu@outlook.com>
2020-08-14 16:15:52 +0200

    ready to deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ puts "Create ingredients"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(open(url).read)
 ingredients["drinks"].each do |ingredient|
-  i = Ingredient.create(name: ingredient["strIngredient1"])
+  i = Ingredient.create(name: ingredient["strIngredient1"].sort)
   puts "create #{i.name}"
 end
